91d26a1aa60e7d33f9a6d9f154fb89b3f1424b0d
Shikky <boughdir72@gmail.com>
2021-11-05 15:35:27 +0100

    New ver.1.1

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ $("form").on("submit", function (e) {
     e.preventDefault()
     $("main").append(`
       <div class="card">
-        <div class="card-body">
-          ${$("input:last").val()} - Le  ${$("input:first").val()}
+        <div class="card-body"><span style="color: ${$( "input:eq(2)" ).val()}">${$("input:eq(1)").val()}</span> - Le  ${$("input:first").val()}
         </div>
       </div>
     `)
